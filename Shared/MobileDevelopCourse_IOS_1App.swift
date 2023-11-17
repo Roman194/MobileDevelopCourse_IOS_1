3d8aef372311f868060965ae66c07f7a4fa14af1
Roman194 <RAZ3113@yandex.ru>
2023-11-17 20:57:41 +0300

    Initial Commit

---
+++ Shared/MobileDevelopCourse_IOS_1App.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevelopCourse_IOS_1App.swift
+//  Shared
+//
+//  Created by Admin on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevelopCourse_IOS_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
